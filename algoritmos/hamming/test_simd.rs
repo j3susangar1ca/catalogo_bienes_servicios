9eb67c84ec08f306a6db58fe9d2eb2f1deeb1b28
j3susangar1ca <j3susangar1ca@gmail.com>
2026-04-24 20:12:09 -0600

    li

--- algoritmos/hamming/test_simd.rs
+++
@@ -1,9 +0,0 @@
-#![feature(portable_simd)]
-use std::simd::u64x4;
-
-fn main() {
-    let a = u64x4::from_array([1, 2, 3, 4]);
-    let b = u64x4::from_array([4, 3, 2, 1]);
-    let c = a ^ b;
-    println!("{:?}", c);
-}
