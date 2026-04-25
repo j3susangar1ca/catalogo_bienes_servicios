5272e2f102deb17624ac06f49baa9cd9eaeb9ff5
j3susangar1ca <j3susangar1ca@gmail.com>
2026-04-24 18:21:22 -0600

    fb

---
+++ target/debug/deps/sorensen_dice_engine-c18fe9d5d1ebf709.d
@@ -0,0 +1,5 @@
+/home/jesuslangarica/catalogo_bienes_servicios/target/debug/deps/sorensen_dice_engine-c18fe9d5d1ebf709.d: algoritmos/sørensen_dice/main.rs
+
+/home/jesuslangarica/catalogo_bienes_servicios/target/debug/deps/libsorensen_dice_engine-c18fe9d5d1ebf709.rmeta: algoritmos/sørensen_dice/main.rs
+
+algoritmos/sørensen_dice/main.rs:
