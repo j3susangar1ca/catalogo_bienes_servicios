db163a218bc7ab79f0b87b4fef3e203386042e9b
j3susangar1ca <j3susangar1ca@gmail.com>
2026-04-24 18:33:14 -0600

    GJ

---
+++ frontend/build/corrosion/required_libs/target/x86_64-unknown-linux-gnu/debug/deps/required_libs-22306ef836724ec0.d
@@ -0,0 +1,5 @@
+/home/jesuslangarica/catalogo_bienes_servicios/frontend/build/corrosion/required_libs/target/x86_64-unknown-linux-gnu/debug/deps/required_libs-22306ef836724ec0.d: lib.rs
+
+/home/jesuslangarica/catalogo_bienes_servicios/frontend/build/corrosion/required_libs/target/x86_64-unknown-linux-gnu/debug/deps/librequired_libs-22306ef836724ec0.a: lib.rs
+
+lib.rs:
